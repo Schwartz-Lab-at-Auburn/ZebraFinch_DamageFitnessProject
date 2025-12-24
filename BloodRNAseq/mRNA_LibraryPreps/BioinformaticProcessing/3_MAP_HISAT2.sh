00b83b13ac1a31237beda2c1068f49917ac7bdf7
Tonia S Schwartz <schwartz.tonia@gmail.com>
2025-12-24 08:12:39 -0600

    Update 3_MAP_HISAT2.sh

--- BloodRNAseq/mRNA_LibraryPreps/BioinformaticProcessing/3_MAP_HISAT2.sh
+++ BloodRNAseq/mRNA_LibraryPreps/BioinformaticProcessing/3_MAP_HISAT2.sh
@@ -2,8 +2,7 @@
 
 HOMEDIR="/scratch/tss0019/ZebraFinch_AllBloodRNAseq"
 CLDATADIR="/scratch/tss0019/ZebraFinch_AllBloodRNAseq/Clean_Paired"
-REFD=/hosted/biosc/SchwartzLab/ReferenceGenomes/ZebraFinch/NCBI/ncbi_dataset/data/HiSat2_Index_NCBI_RefSeq_GCF_0487719
-95.1          
+REFD=/hosted/biosc/SchwartzLab/ReferenceGenomes/ZebraFinch/NCBI/ncbi_dataset/data/HiSat2_Index_NCBI_RefSeq_GCF_048771995.1          
 REF=ZF_NCBI_GCF_048771995.1_index 
 MAPDIR="${HOMEDIR}/MAP_HISAT2"
 
