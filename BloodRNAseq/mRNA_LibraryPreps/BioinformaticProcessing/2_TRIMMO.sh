a38c1d70d7d9f0fc63fa95a55386df3cb7c5db37
Tonia S Schwartz <schwartz.tonia@gmail.com>
2025-12-25 23:06:14 -0600

    Update 2_TRIMMO.sh

--- BloodRNAseq/mRNA_LibraryPreps/BioinformaticProcessing/2_TRIMMO.sh
+++ BloodRNAseq/mRNA_LibraryPreps/BioinformaticProcessing/2_TRIMMO.sh
@@ -19,4 +19,4 @@ java -jar /tools/trimmomatic-0.39/trimmomatic-0.39.jar \
          PE -threads 6 -phred33 \
          "${DATADIR}"/"${sample}_1.fq.gz"  "${DATADIR}"/"${sample}_2.fq.gz"  \
        	 "${CLDATADIR}"/"${sample}_1_paired.fq.gz" "${CLDATADIR}"/"${sample}_1_unpaired.fq.gz" "${CLDATADIR}"/"${sample}_2_paired.fq.gz" "${CLDATADIR}"/"${sample}_2_unpaired.fq.gz" \
-       	 ILLUMINACLIP:"${USEFUL}"/TruSeq3-PE_Additions.fa:2:35:10 HEADCROP:0 LEADING:20 TRAILING:20 SLIDINGWINDOW:6:20 MINLEN:36
+       	 ILLUMINACLIP:"${USEFUL}"/TruSeq3-PE-2_Additions.fa:2:35:10 HEADCROP:0 LEADING:20 TRAILING:20 SLIDINGWINDOW:6:20 MINLEN:36
